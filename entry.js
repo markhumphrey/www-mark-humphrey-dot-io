9bf789ee5ec623a18b1bad1612318f5b8d5ae810
Mark Humphrey <Mark.Humphrey@gmail.com>
2015-11-23 17:57:03 -0500

    Initial commit of angular seed boilerplate. Depdency management consolidate into npm

---
+++ entry.js
@@ -0,0 +1,6 @@
+require("angular");
+require("angular-loader");
+require("angular-mocks");
+require("angular-route");
+//require("modernizr");
+require("normalize-css");
---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : './',
+
+    files : [
+      'dist/bundle.js',
+      'app/components/**/*.js',
+      'app/view*/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
