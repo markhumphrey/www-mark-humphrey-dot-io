edb5a843ac5bd2dbaaf90e5715f8b35cceda67db
Mark Humphrey <Mark.Humphrey@gmail.com>
2015-11-23 18:42:13 -0500

    Switch to ui-router from ng-router

--- entry.js
+++ entry.js
@@ -2,5 +2,6 @@ require("angular");
 require("angular-loader");
 require("angular-mocks");
 require("angular-route");
+require("angular-ui-router");
 //require("modernizr");
 require("normalize-css");
