d55ffa8691f7bc06d1dacc021e2f487416beb470
Mark Humphrey <Mark.Humphrey@gmail.com>
2015-11-23 18:51:45 -0500

    Cleanup lingering ng-router references

--- app/app.js
+++ app/app.js
@@ -9,15 +9,4 @@ angular.module('myApp', [
 ]).
 config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/view1');
-
-  $stateProvider
-  .state('view1', {
-    url: "/view1",
-    templateUrl: "view1/view1.html"
-  })
-  .state('view2', {
-    url: "/view2",
-    templateUrl: "view2/view2.html"
-  });
-
 });
