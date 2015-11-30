9efb5351bdd8a9a5cb58db3dc6b9d914a9a1fe87
Mark Humphrey <Mark.Humphrey@gmail.com>
2015-11-29 19:06:47 -0500

    Adding proxy server middleware for gulp-connect for /api/ resources

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,28 @@ gulp.task('connect', function() {
   connect.server({
     root: [__dirname + "/app/"],
     port: 8000,
+    livereload: true,
     middleware: function(connect, opt) {
-      return [ historyApiFallback() ];
+      return [
+        (function() {
+          var url = require('url');
+          var proxy = require('proxy-middleware');
+          //var options = url.parse('http://localhost:8088/api');
+          var options = url.parse('http://192.168.99.100:8080/');
+          options.route = '/api';
+          return proxy(options);
+        })(),
+        historyApiFallback()
+      ];
     }
   });
 });
 
 gulp.task('browserify', function() {
   // Grabs the app.js file
-  return browserify('./app/app.js', {debug : true})
+  return browserify('./app/app.js', {
+      debug: true
+    })
     .bundle()
     .pipe(source('bundle.js'))
     // saves it the public/js/ directory
@@ -25,8 +38,8 @@ gulp.task('browserify', function() {
 });
 
 gulp.task('sass', function() {
-    return sass('app/static/sass/app.scss')
-        .pipe(gulp.dest('app/dist/css'));
+  return sass('app/static/sass/app.scss')
+    .pipe(gulp.dest('app/dist/css'));
 });
 
 gulp.task('watch', function() {
