7154452b6c710c9275f39c35372a3f623d8dd979
Mark Humphrey <Mark.Humphrey@gmail.com>
2016-01-04 21:44:11 -0500

    First pass at creating gulp tasks to generate all build artifacts and move to dist dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,74 @@
 var gulp = require('gulp');
-var connect = require('gulp-connect');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var clean = require('gulp-clean');
 var sass = require('gulp-ruby-sass');
 var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var historyApiFallback = require('connect-history-api-fallback');
+//var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+//var gutil = require('gulp-util');
 
-gulp.task('connect', function() {
-  connect.server({
-    root: [__dirname + "/app/"],
-    port: 8000,
-    //livereload: true,
-    middleware: function(connect, opt) {
-      return [
-        (function() {
-          var url = require('url');
-          var proxy = require('proxy-middleware');
-          //var options = url.parse('http://localhost:8088/api');
-          var options = url.parse('http://192.168.99.100:8080/');
-          options.route = '/api';
-          return proxy(options);
-        })(),
-        historyApiFallback()
-      ];
-    }
-  });
+var paths = {
+ dist: 'dist/',
+ scriptEntry: 'app/app.js',
+ scripts: ['app/app.js', 'app/components/**/*.js', 'app/shared/**/*.js'],
+ libs: ['node_modules/**/*.js'],
+ styles: ['app/static/sass/**/*'],
+ styleEntry: 'app/static/sass/app.scss',
+ html: ['app/**/*.html', '!app/static/'],
+ images: ['app/static/images/**/*'],
+ extras: [],
+};
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+  gulp.watch(paths.html, ['build-html']);
+  gulp.watch(paths.images, ['build-images']);
+  gulp.watch(paths.scripts, ['build-scripts']);
+  gulp.watch(paths.styles, ['build-styles']);
 });
 
-gulp.task('browserify', function() {
-  // Grabs the app.js file
-  return browserify('./app/app.js', {
-      debug: true
-    })
-    .bundle()
+gulp.task('build', ['build-html', 'build-images', 'build-scripts', 'build-styles']);
+
+gulp.task('build-clean', function() {
+  gulp.src(paths.dist)
+  .pipe(clean());
+});
+
+gulp.task('build-html', function() {
+ gulp.src(paths.html)
+ .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('build-images', function() {
+  gulp.src(paths.images)
+  .pipe(gulp.dest(paths.dist + 'images/'));
+});
+
+gulp.task('build-scripts', ['browserify']);
+
+gulp.task('build-styles', ['sass']);
+
+gulp.task('browserify', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: paths.scriptEntry,
+    debug: true
+  });
+
+  return b.bundle()
     .pipe(source('bundle.js'))
-    // saves it the public/js/ directory
-    .pipe(gulp.dest('./app/dist/js/'));
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        //.pipe(uglify())
+        //.on('error', gutil.log)
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dist + 'js/'));
 });
 
 gulp.task('sass', function() {
-  return sass('app/static/sass/app.scss')
-    .pipe(gulp.dest('app/dist/css'));
+  sass(paths.styleEntry)
+    .pipe(gulp.dest(paths.dist + 'css/'));
 });
-
-gulp.task('watch', function() {
-  gulp.watch(['app/**/*.js',
-    '!app/dist/**'
-  ], ['browserify']);
-  gulp.watch('app/static/sass/**', ['sass']);
-});
-
-gulp.task('default', ['connect', 'watch']);
